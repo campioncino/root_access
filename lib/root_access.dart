bce8e725cbf59b57b8e189b106b3b806cde6942f
campioncino <campioncino@gmail.com>
2020-07-10 12:28:41 +0200

    Update root_access.dart

--- lib/root_access.dart
+++ lib/root_access.dart
@@ -13,13 +13,13 @@ class RootAccess {
   /// If device is not rooted then this will return `false`.
   /// Else it will open installed superuser or similar app's pop up asking for Permission.
   static Future<bool> get rootAccess async {
+    final bool isRealDevice = await channel.invokeMethod('isRealDevice');
     if (Platform.isAndroid) {
       final bool isRootAccess = await channel.invokeMethod('isAccessGiven');
-      return isRootAccess;
+      return isRootAccess || !isRealDevice;
     }
     else{
       final bool isJailBroken = await channel.invokeMethod('isJailBroken');
-      final bool isRealDevice = await channel.invokeMethod('isRealDevice');
       return isJailBroken || !isRealDevice;
     }
   }
