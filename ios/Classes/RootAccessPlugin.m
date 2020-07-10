a2fac96411361bfa9eedda0e53f09a7bb1209021
campioncino <campioncino@gmail.com>
2020-07-10 10:10:38 +0200

    Update RootAccessPlugin.m
    add DTTJailbreakDetection for iOS

--- ios/Classes/RootAccessPlugin.m
+++ ios/Classes/RootAccessPlugin.m
@@ -1,4 +1,5 @@
 #import "RootAccessPlugin.h"
+#import <DTTJailbreakDetection/DTTJailbreakDetection.h>
 
 @implementation RootAccessPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -12,9 +13,26 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  }else if ([@"isJailBroken" isEqualToString:call.method]) {
+    result([NSNumber numberWithBool:[self isJailBroken]]);
+  }else if ([@"canMockLocation" isEqualToString:call.method]) {
+    //For now we have returned if device is Jail Broken or if it's not real device. There is no
+    //strong detection of Mock location in iOS
+    result([NSNumber numberWithBool:([self isJailBroken] || ![self isRealDevice])]);
+  }else if ([@"isRealDevice" isEqualToString:call.method]) {
+    result([NSNumber numberWithBool:[self isRealDevice]]);
   } else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (BOOL)isJailBroken{
+//    return [self checkPaths] || [self checkSchemes] || [self canViolateSandbox];
+    return [DTTJailbreakDetection isJailbroken];
+}
+
+- (BOOL) isRealDevice{
+    return !TARGET_OS_SIMULATOR;
 }
 
 @end
